928970960101b511ee5bd711910302f743354d0d
Bert van Hoekelen <bert@magwel.nl>
2021-01-15 15:51:07 +0100

    :arrow_up: Upgrade Homey apps sdk to version 3.0

--- api.js
+++ api.js
@@ -1,28 +1,7 @@
-const Homey = require('homey');
+module.exports = {
+  async updateLockState({ homey, params, query, body }) {
+    var parsedJson = body;
 
-module.exports = [
-  {
-    method: 'POST',
-    path: '/',
-    public: true,
-    fn: function( args, callback ) {
-      console.log(args);
-
-      Homey.ManagerDrivers.getDriver('touch-smart-lock').getDevices().forEach((device) => {
-        console.log(device.getData());
-      })
-
-      try {
-        let touchSmartLock = Homey.ManagerDrivers
-          .getDriver("touch-smart-lock")
-          .getDevice({"id": args.body.lock_id});
-
-        touchSmartLock.setState(args.body.requested_state, args.body.key_account_email);
-      } catch (error) {
-        callback(error, false);
-      }
-
-      callback(null, 'success');
-    }
-  }
-]
+    return homey.app.setLockState(parsedJson.lock_id, parsedJson.requested_state, parsedJson.key_account_email);
+  },
+};
